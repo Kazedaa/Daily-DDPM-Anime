4ef2fe7f4d834723471e46cc239aff4bee3a44c5
Hemang J Jamadagni <120291477+Kazedaa@users.noreply.github.com>
2024-06-16 08:33:14 +0530

    added latest_post,png

--- post.py
+++ post.py
@@ -10,7 +10,7 @@ cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
 num_posts = 1
 
 for i in range(num_posts):
-    path = generate(f"images/{datetime.today().date()}.png")
+    path = generate(f"latest_post.png")
     caption = '''Ah, hello there. You’ve caught me at an interesting time. I was just contemplating the beauty of life's fleeting moments... and how they often end in such fascinating ways. But enough about my existential musings. How can I assist you today? Perhaps you're looking for some advice, a philosophical debate, or maybe even a... partner in a double suicide? Just kidding, of course. Or am I?
 
 What brings you to the realm of the Armed Detective Agency?'''
