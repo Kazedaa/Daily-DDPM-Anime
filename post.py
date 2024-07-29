029d82f36453204d368054db47701bf0c6f40089
Hemang J Jamadagni <120291477+Kazedaa@users.noreply.github.com>
2024-07-29 11:20:49 +0530

    Update post.py

--- post.py
+++ post.py
@@ -7,7 +7,7 @@ ACCOUNT_PASSWORD = os.environ["INSTAGRAM_PASSWORD"]
 
 cl = Client()
 cl.set_proxy("http://94.23.204.27:3128")
-cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
+# cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
 print("Sign Successfull...")
 num_posts = 1
 
